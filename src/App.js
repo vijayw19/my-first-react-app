eb9e18f64579369090b62e00e17135e2fd1d8145
Yadav <vijay.d.yadav@capgemini.com>
2020-03-24 16:54:43 -0400

    first push

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>welcome to first app...</h1>
+	  <h6>feeling good</h6>
+    </div>
+  );
+}
+
+export default App;
